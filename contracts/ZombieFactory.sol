7e5b799cc639119de78865b49108348bdb0e97ad
onee <wangyu@onee.io>
2021-05-30 23:53:59 +0800

    :construction: add zombie helper contract

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -38,8 +38,8 @@ contract ZombieFactory is Ownable {
     Zombie[] public zombies;
     // 僵尸拥有者映射：僵尸ID => 拥有者
     mapping (uint => address) public zombieToOwner;
-    // 地址拥有僵尸数量映射：拥有者 => 僵尸数量
-    mapping (address => uint) ownerZombieCount;
+    // 地址拥有僵尸数组映射：拥有者 => 僵尸ID数组
+    mapping (address => uint[]) ownerZombies;
     // 僵尸喂养次数映射：僵尸ID => 喂养次数
     mapping (uint => uint) zombieFeedTimes;
 
@@ -49,7 +49,7 @@ contract ZombieFactory is Ownable {
     // 创建僵尸
     function createZombie(string memory name) public {
         // 校验每个账户只能免费生成一只僵尸
-        require(ownerZombieCount[msg.sender] == 0, "You already have a zombie.");
+        require(ownerZombies[msg.sender].length == 0, "You already have a zombie.");
         uint randomDna = _generateRandomDna(name);
         randomDna = randomDna - randomDna % 10; // 将基因最后一位改为0，标记为免费僵尸
         _createZombie(name, randomDna);
@@ -58,12 +58,17 @@ contract ZombieFactory is Ownable {
     // 购买僵尸
     function buyZombie(string memory name) public payable {
         // 校验账户目前没有僵尸
-        require(ownerZombieCount[msg.sender] > 0, "You can create a zombie for free.");
+        require(ownerZombies[msg.sender].length > 0, "You can create a zombie for free.");
         // 校验金额大于等于僵尸售价
         require(msg.value >= zombiePrice, "Your balance is not enough.");
         uint randomDna = _generateRandomDna(name);
         randomDna = randomDna - randomDna % 10 + 1; // 将基因最后一位改为1，标记为购买僵尸
         _createZombie(name, randomDna);
+    }
+
+    // 修改僵尸售价（只有合约拥有者才可以修改）
+    function setZombiePrice(uint price) external onlyOwner {
+        zombiePrice = price;
     }
 
     // 生成随机基因序列（此随机算法不安全）
@@ -78,14 +83,9 @@ contract ZombieFactory is Ownable {
         uint zombieId = zombies.length - 1;
         // 设定僵尸拥有者并更新数量
         zombieToOwner[zombieId] = msg.sender;
-        ownerZombieCount[msg.sender] = ownerZombieCount[msg.sender].add(1);
+        ownerZombies[msg.sender].push(zombieId);
         zombieCount = zombieCount.add(1);
         // 触发僵尸创建事件
         emit NewZombie(zombieId, name, dna);
-    }
-
-    // 修改僵尸售价（只有合约拥有者才可以修改）
-    function _setZombiePrice(uint price) external onlyOwner {
-        zombiePrice = price;
     }
 }
---
+++ contracts/ZombieHelper.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "./ZombieFactory.sol";
+
+// 僵尸助手合约
+contract ZombieHelper is ZombieFactory {
+    using SafeMath for uint256;
+    
+    // 升级费用
+    uint public levelUpFee = 0.0001 ether;
+
+    // 判断僵尸大于指定等级
+    modifier aboveLevel(uint level, uint zombieId) {
+        require(zombies[zombieId].level >= level, "Your zombie level is less than level.");
+        _;
+    }
+
+    // 判断调用者为僵尸的拥有者
+    modifier onlyOwnerOf(uint zombieId) {
+        require(msg.sender == zombieToOwner[zombieId], "This zombie doesn't belong to you.");
+        _;
+    }
+
+    // 设置升级费（只有合约拥有者才可以修改）
+    function setLevelUpFee(uint fee) external onlyOwner {
+        levelUpFee = fee;
+    }
+
+    // 付费升级
+    function levelUp(uint zombieId) external payable {
+        require(msg.value >= levelUpFee, "Your balance is not enough.");
+        zombies[zombieId].level = zombies[zombieId].level.add(1); // 使用"zombies[zombieId].level++;"也可以
+    }
+
+    // 僵尸改名（需为自己所拥有的僵尸并且等级至少2级）
+    function changeName(uint zombieId, string calldata name) external aboveLevel(2, zombieId) onlyOwnerOf(zombieId) {
+        zombies[zombieId].name = name;
+    }
+
+    // 获取地址所拥有的僵尸数组
+    function getZombiesByOwner(address owner) external view returns (uint[] memory) {
+        return ownerZombies[owner];
+    }
+
+    // 触发冷却
+    function _triggerCooldown(Zombie storage zombie) internal {
+        // 冷却时间到次日凌晨0点
+        zombie.readyTime = (block.timestamp + cooldownTime) - (block.timestamp + cooldownTime) % 1 days;
+    }
+
+    // 判断僵尸是否完成冷却
+    function _isReady(Zombie storage zombie) internal view returns (bool) {
+        return block.timestamp >= zombie.readyTime;
+    }
+
+    // 僵尸合体 产生新僵尸
+    function _multiply(uint zombieId, uint targetDna) internal onlyOwnerOf(zombieId) {
+        Zombie storage zombie = zombies[zombieId];
+        require(_isReady(zombie), "Your zombie are cooldown");
+        // 取两个基因序列的平均数为新的基因序列
+        targetDna = targetDna % dnaModulus; // 确保目标基因序列满足格式
+        uint newDna = (zombie.dna.add(targetDna)).div(2);
+        newDna = newDna - newDna % 10 + 9; // 将基因最后一位改为9，标记为合体僵尸
+        _createZombie("NoName", newDna);
+    }
+}
